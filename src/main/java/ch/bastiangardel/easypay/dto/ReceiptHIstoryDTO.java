36ff902704d09c329296f478f68fbaa694fe7a82
bastiangardel <bastiangardel@gmail.com>
2016-06-28 15:19:16 +0200

    update

--- src/main/java/ch/bastiangardel/easypay/dto/ReceiptHIstoryDTO.java
+++ src/main/java/ch/bastiangardel/easypay/dto/ReceiptHIstoryDTO.java
@@ -2,6 +2,9 @@ package ch.bastiangardel.easypay.dto;
 
 import ch.bastiangardel.easypay.model.Receipt;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 /**
  * Created by bastiangardel on 27.06.16.
  */
@@ -9,6 +12,7 @@ public class ReceiptHistoryDTO {
     private Double amount;
     private Long id;
     private String payBy;
+    private String created;
 
     public Double getAmount() {
         return amount;
@@ -34,12 +38,23 @@ public class ReceiptHistoryDTO {
         this.payBy = payBy;
     }
 
-    public ReceiptHistoryDTO modelToDto(Receipt receipt)
-    {
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public ReceiptHistoryDTO modelToDto(Receipt receipt) {
         amount = receipt.getAmount();
         id = receipt.getId();
         payBy = receipt.getPaiyedBy().getName();
 
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        created  = df.format(receipt.getCreated());
+
         return this;
     }
+
 }
